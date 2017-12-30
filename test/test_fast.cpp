abcf4a07b88e4f56166a6cb91140b85864fae658
Justin Bassett <jbassett271@gmail.com>
2017-12-29 17:36:14 -0800

    Fix the doc-bug in #1. Fix bug where assertions are removed rather than replaced with void expression.

--- test/test_fast.cpp
+++ test/test_fast.cpp
@@ -14,14 +14,14 @@ TEST_CASE("")
 
     SECTION("fast assert")
     {
-        REQUIRE_THROWS_AS([] { QCX_ASSERT_FAST(false); }(), qcx::assertion_error);
+        REQUIRE_THROWS_AS(QCX_ASSERT_FAST(false), qcx::assertion_error);
     }
     SECTION("regular assert")
     {
-        REQUIRE_NOTHROW([] { QCX_ASSERT(false); }());
+        REQUIRE_NOTHROW(QCX_ASSERT(false));
     }
     SECTION("safe assert")
     {
-        REQUIRE_NOTHROW([] { QCX_ASSERT_SAFE(false); }());
+        REQUIRE_NOTHROW(QCX_ASSERT_SAFE(false));
     }
 }
